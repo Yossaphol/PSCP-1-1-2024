9654ecb72690d99a7fe47647c0dc901c510b3cfa
Yossaphol <yossaphol3502@gmail.com>
2024-07-09 13:15:37 +0700

    update

---
+++ week 1/Gift IIv2v.py
@@ -0,0 +1,28 @@
+"""gift II v2"""
+def main():
+    """main"""
+    num1 = int(input())
+    num2 = int(input())
+    num3 = int(input())
+    num4 = int(input())
+    num5 = int(input())
+    num6 = int(input())
+    num7 = int(input())
+    num8 = int(input())
+    if not num1 % 2:
+        print(num1)
+    if not num2 % 2:
+        print(num2)
+    if not num3 % 2:
+        print(num3)
+    if not num4 % 2:
+        print(num4)
+    if not num5 % 2:
+        print(num5)
+    if not num6 % 2:
+        print(num6)
+    if not num7 % 2:
+        print(num7)
+    if not num8 % 2:
+        print(num8)
+main()
