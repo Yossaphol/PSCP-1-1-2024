d239e2d26c4082373f3d11d21df49267535e1a13
Yossaphol <yossaphol3502@gmail.com>
2024-10-07 12:28:46 +0700

    Update help.py

--- help.py
+++ help.py
@@ -1,16 +1,4 @@
-"""Heads and Legs"""
-def main(head, leg):
-    """main"""
-    if leg < head * 2 or leg % 2 or leg > head * 4:
-        print('Impossible')
-    else:
-        for bird in range(head+1):
-            rabbit = head - bird
-            if (2 * bird) + (4 * rabbit) == leg:
-                print(rabbit, bird)
-                break
-        # print("---------------")
-        # rabbit = leg // 4 if math.ceil(leg / head) >= 4 else 0
-        # bird = (leg - (rabbit * 4)) // 2
-        # print(rabbit, bird)
-main(int(input()), int(input()))
+print(True and -2)
+print(-2 and True)
+print(1 or 0)
+print(0 or -1)
