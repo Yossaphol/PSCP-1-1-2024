0f7732d6d988204ffd6d1647bf2e8c7b3cdda161
Yossaphol <yossaphol3502@gmail.com>
2024-10-08 14:44:43 +0700

    Update help.py

--- help.py
+++ help.py
@@ -2,3 +2,13 @@ print(True and -2)
 print(-2 and True)
 print(1 or 0)
 print(0 or -1)
+
+i = -1
+while i > -8:
+    print(i)
+    i -= 3
+
+print((10, 'hello') > (9, 'moon'))
+print(('moon', 10) > ('world', 20))
+print(('10', 'world') > ('2', 'world'))
+print((10, 11, 12, 1) > (10, 11, 12, 2))
