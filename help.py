07cd67dcbefbb5f4bfcd95e3f882f13d40f7f15e
Yossaphol <yossaphol3502@gmail.com>
2024-10-14 01:29:12 +0700

    Update help.py

--- help.py
+++ help.py
@@ -1,17 +1,27 @@
-print(True and -2)
-print(-2 and True)
-print(1 or 0)
-print(0 or -1)
+# print(True and -2)
+# print(-2 and True)
+# print(1 or 0)
+# print(0 or -1)
 
-i = -1
-while i > -8:
-    print(i)
-    i -= 3
+# i = -1
+# while i > -8:
+#     print(i)
+#     i -= 3
 
-print((10, 'hello') > (9, 'moon'))
-print(('moon', 10) > ('world', 20))
-print(('10', 'world') > ('2', 'world'))
-print((10, 11, 12, 1) > (10, 11, 12, 2))
+# print((10, 'hello') > (9, 'moon'))
+# print(('moon', 10) > ('world', 20))
+# print(('10', 'world') > ('2', 'world'))
+# print((10, 11, 12, 1) > (10, 11, 12, 2))
 
-ts = 'abcdefg'
-print(ts[::-1])
+# ts = 'abcdefg'
+# print(ts[::-1])
+
+muy = []
+for i in range(5):
+    n = input()
+    muy.append(n)
+
+muy.sort(key=len, reverse=True)
+for i in range(len(muy)):
+    print(muy[i])
+print(muy)
